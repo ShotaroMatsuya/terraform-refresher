05baab4ab491d4bfcfc408d5ed15269c1d94dc44
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-09-18 19:34:31 +0900

    create: set start point

---
+++ 11ALB-basic/terraform-manifests/c7-04-ec2instance-private.tf
@@ -0,0 +1,22 @@
+# EC2 Instances that will be created in VPC Private Subnets
+module "ec2_private" {
+  depends_on = [
+    module.vpc
+  ]
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "4.1.4"
+
+  for_each = local.multiple_instances
+
+  name = "${var.environment}-vm-${each.value.num_suffix}"
+  ami = data.aws_ami.amzlinux2.id
+  instance_type = each.value.instance_type
+  user_data = file("${path.module}/app1-install.sh")
+  key_name = var.instance_keypair
+
+  vpc_security_group_ids = [module.private_sg.security_group_id]
+  subnet_id = each.value.subnet_id
+  # deprecated
+  # subnet_id = module.vpc.private_subnets[0]
+  tags = local.common_tags
+}
