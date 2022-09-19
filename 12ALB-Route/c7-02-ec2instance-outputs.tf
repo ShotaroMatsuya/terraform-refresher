ec46502e3d0592a5587370bc220a9d3225965af8
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-09-19 12:10:12 +0900

    UPDATE: divide private Ec2 into 2

--- 12ALB-Route/c7-02-ec2instance-outputs.tf
+++ 12ALB-Route/c7-02-ec2instance-outputs.tf
@@ -8,20 +8,42 @@ output "ec2_bastion_public_ip" {
   description = "List of Public ip address assigned to the instances"
   value       = module.ec2_public.public_ip
 }
-# Private EC2 Instances
-output "ec2_private_instance_ids" {
+# App1 - Private EC2 Instances
+## ec2_private_instance_ids
+output "app1_ec2_private_instance_ids" {
   description = "List of IDs of instances"
   # value       = module.ec2_private.id
   # for_eachで回したので変更
   value       = toset([
-    for myec2vm in module.ec2_private : myec2vm.id
+    for myec2vm in module.ec2_private_app1 : myec2vm.id
   ])
+
 }
-output "ec2_private_ip" {
+## ec2_private_ip
+output "app1_ec2_private_ip" {
   description = "List of private ip address assigned to the instances"
   # value       = module.ec2_private.private_ip
   # for_eachで回したので変更
   value       = toset([
-    for myec2vm in module.ec2_private : myec2vm.private_ip
+    for myec2vm in module.ec2_private_app1 : myec2vm.private_ip
+  ])
+}
+# App2 - Private EC2 Instances
+## ec2_private_instance_ids
+output "app2_ec2_private_instance_ids" {
+  description = "List of IDs of instances"
+  # value       = module.ec2_private.id
+  # for_eachで回したので変更
+  value       = toset([
+    for myec2vm in module.ec2_private_app2 : myec2vm.id
+  ])
+}
+## ec2_private_ip
+output "app2_ec2_private_ip" {
+  description = "List of private ip address assigned to the instances"
+  # value       = module.ec2_private.private_ip
+  # for_eachで回したので変更
+  value       = toset([
+    for myec2vm in module.ec2_private_app2 : myec2vm.private_ip
   ])
 }
