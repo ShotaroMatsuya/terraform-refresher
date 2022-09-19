5b4b03abb525a44302c8c3b4d395a0bc46dd1401
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-09-19 18:05:19 +0900

    UPDATE: implement custom header  & QS routing

--- 12ALB-Route/terraform-manifest/c12-route53-dnsregistration.tf
+++ 12ALB-Route/terraform-manifest/c12-route53-dnsregistration.tf
@@ -11,10 +11,10 @@ resource "aws_route53_record" "default_dns" {
   }  
 }
 
-## App1 DNS
+## Testing Host Header - Redirect to External Site from ALB HTTPS Listener Rules
 resource "aws_route53_record" "app1_dns" {
   zone_id = data.aws_route53_zone.mydomain.zone_id 
-  name    = var.app1_dns_name
+  name    = "azure-aks.smat710.tk"
   type    = "A"
   alias {
     name                   = module.alb.lb_dns_name
@@ -24,14 +24,28 @@ resource "aws_route53_record" "app1_dns" {
 }
 
 
-## App2 DNS
-resource "aws_route53_record" "app2_dns" {
-  zone_id = data.aws_route53_zone.mydomain.zone_id 
-  name    = var.app2_dns_name
-  type    = "A"
-  alias {
-    name                   = module.alb.lb_dns_name
-    zone_id                = module.alb.lb_zone_id
-    evaluate_target_health = true
-  }  
-}
+
+# ## App1 DNS
+# resource "aws_route53_record" "app1_dns" {
+#   zone_id = data.aws_route53_zone.mydomain.zone_id 
+#   name    = var.app1_dns_name
+#   type    = "A"
+#   alias {
+#     name                   = module.alb.lb_dns_name
+#     zone_id                = module.alb.lb_zone_id
+#     evaluate_target_health = true
+#   }  
+# }
+
+
+# ## App2 DNS
+# resource "aws_route53_record" "app2_dns" {
+#   zone_id = data.aws_route53_zone.mydomain.zone_id 
+#   name    = var.app2_dns_name
+#   type    = "A"
+#   alias {
+#     name                   = module.alb.lb_dns_name
+#     zone_id                = module.alb.lb_zone_id
+#     evaluate_target_health = true
+#   }  
+# }
