efdab7dc37ae2d2b5f80dc2ea0539afd8fd97f46
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-09-25 19:55:19 +0900

    UPDATE: create ec2 launch-template

--- 14ASG/terraform-manifests/c13-02-autoscaling-launchtemplate-resource.tf
+++ 14ASG/terraform-manifests/c13-02-autoscaling-launchtemplate-resource.tf
@@ -1,16 +1,37 @@
-# AWS IAM Service Linked Role for Autoscaling Group
-resource "aws_iam_service_linked_role" "autoscaling" {
-  aws_service_name = "autoscaling.amazonaws.com"
-  description      = "A service linked role for autoscaling"
-  custom_suffix    = local.name
-
-  # Sometimes good sleep is required to have some IAM resources created before they can be used
-  provisioner "local-exec" {
-    command = "sleep 10"
+# Launch Template Resource
+resource "aws_launch_template" "my_launch_template" {
+  name = "my-launch-template"
+  description = "My Launch template"
+  image_id = data.aws_ami.amzlinux2.id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [ module.private_sg.security_group_id ]
+  key_name = var.instance_keypair
+  user_data = filebase64("${path.module}/app1-install.sh")
+  ebs_optimized = true 
+  #default_version = 1
+  update_default_version = true 
+  block_device_mappings {
+    device_name = "/dev/sda1"
+    ebs {
+      # volume_size = 10      
+      volume_size = 20 # LT Update Testing - Version 2 of LT              
+      delete_on_termination = true
+      volume_type = "gp2" # default  is gp2 
+    }
   }
-}
-
-# Output AWS IAM Service Linked Role
-output "service_linked_role_arn" {
-    value   = aws_iam_service_linked_role.autoscaling.arn
+  # # ADD
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_tokens                 = "optional"
+    http_put_response_hop_limit = 32
+  }
+  monitoring {
+    enabled = true
+  }   
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "myasg"
+    }
+  }  
 }
--- 14ASG/terraform-manifests/c13-03-autoscaling-resource.tf
+++ 14ASG/terraform-manifests/c13-03-autoscaling-resource.tf
@@ -1,100 +1,31 @@
-# Autoscaling with Launch Configuration - Both created at a time
-module "autoscaling" {
-  source  = "terraform-aws-modules/autoscaling/aws"
-  version = "6.5.2"
+# Autoscaling Group Resource
+resource "aws_autoscaling_group" "my_asg" {
+  depends_on = [ module.vpc ] # VERY VERY IMPORTANT else userdata webserver provisioning will fail
+  name_prefix = "myasg-"
+  desired_capacity = 2
+  max_size = 10
+  min_size = 2
+  vpc_zone_identifier = module.vpc.private_subnets
+  target_group_arns = module.alb.target_group_arns
+  health_check_type = "EC2"
+  #health_check_grace_period = 300 # default is 300 seconds
 
-  # Autoscaling group
-  name            = "${local.name}-myasg1"
-  use_name_prefix = false
-
-  min_size                  = 2
-  max_size                  = 10
-  # desired_capacity          = 2
-  desired_capacity          = 3  # Changed for testing Instance Refresh as part of Step-10 
-  wait_for_capacity_timeout = 0
-  health_check_type         = "EC2"
-  vpc_zone_identifier       = module.vpc.private_subnets
-  service_linked_role_arn   = aws_iam_service_linked_role.autoscaling.arn
-  # Associate ALB with ASG
-  target_group_arns         = module.alb.target_group_arns
-
-  # ASG Lifecycle Hooks
-  initial_lifecycle_hooks = [
-    {
-      name                 = "ExampleStartupLifeCycleHook"
-      default_result       = "CONTINUE"
-      heartbeat_timeout    = 60
-      lifecycle_transition = "autoscaling:EC2_INSTANCE_LAUNCHING"
-      # This could be a rendered data resource
-      notification_metadata = jsonencode({ "hello" = "world" })
-    },
-    {
-      name                 = "ExampleTerminationLifeCycleHook"
-      default_result       = "CONTINUE"
-      heartbeat_timeout    = 180
-      lifecycle_transition = "autoscaling:EC2_INSTANCE_TERMINATING"
-      # This could be a rendered data resource
-      notification_metadata = jsonencode({ "goodbye" = "world" })
-    }
-  ]
-
-  # ASG Instance Referesh
-  instance_refresh = {
+  launch_template {
+    id = aws_launch_template.my_launch_template.id 
+    version = aws_launch_template.my_launch_template.latest_version
+  }
+  # Instance Refresh
+  instance_refresh {
     strategy = "Rolling"
-    preferences = {
-      min_healthy_percentage = 50
+    preferences {
+      # instance_warmup = 300 # Default behavior is to use the Auto Scaling Groups health check grace period value
+      min_healthy_percentage = 50            
     }
-    triggers = ["tag", "desired_capacity"] # Desired Capacity here added for demostrating the Instance Refresh scenario
+    triggers = [ "desired_capacity" ] # You can add any argument from ASG here, if those has changes, ASG Instance Refresh will trigger   
   }
-
-  # ASG Launch configuration
-  launch_template_name   = "${local.name}-mylc1"
-  update_default_version      = true
-
-  image_id          = data.aws_ami.amzlinux2.id
-  instance_type     = var.instance_type
-  key_name          = var.instance_keypair
-  user_data         = filebase64("${path.module}/app1-install.sh")
-  ebs_optimized     = true
-  enable_monitoring = true
-
-  security_groups             = [module.private_sg.security_group_id]
-
-  # Add Spot Instances, which creates Spot Requests to get instances at the price listed (Optional argument)
-  # spot_price        = "0.014"
-  #spot_price        = "0.016" # Change for Instance Refresh test
-  # instance_market_options = {
-  #   market_type = "spot"
-  # }
-
-  block_device_mappings = [
-    {
-      # Root volume
-      device_name = "/dev/xvda"
-      no_device   = 0
-      ebs = {
-        delete_on_termination = true
-        encrypted             = true
-        volume_size           = 15
-        volume_type           = "gp2"
-      }
-    }, 
-    {
-      device_name = "/dev/xvdz"
-      no_device   = 1
-      ebs = {
-        delete_on_termination = true
-        encrypted             = true
-        volume_size           = 20
-        volume_type           = "gp2"
-      }
-    }
-  ]
-  metadata_options = {
-    http_endpoint               = "enabled"
-    http_tokens                 = "optional" # At production grade you can change to "required", for our example if is optional we can get the content in metadata.html
-    http_put_response_hop_limit = 32
+  tag {
+    key                 = "Owners"
+    value               = "Web-Team"
+    propagate_at_launch = true
   }
-
-  tags        = local.asg_tags
 }
