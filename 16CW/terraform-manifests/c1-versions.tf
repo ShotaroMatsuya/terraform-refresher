641f7b5a5ed538e97a498d1ae45f1f3be37caf27
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-09-29 02:24:56 +0900

    ADD: create new prj for CW

---
+++ 16CW/terraform-manifests/c1-versions.tf
@@ -0,0 +1,29 @@
+# Terraform Block
+terraform {
+  required_version = "~> 1.2"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+    null = {
+      source = "hashicorp/null"
+      version = "~> 3.0.0"
+    }
+    random = {
+      source ="hashicorp/random"
+      version ="~> 3.0"
+    }
+  }
+}
+
+# Provider Block
+provider "aws" {
+  region = var.aws_region
+  profile = "default"
+}
+
+# Create Random Pet Resource 
+resource "random_pet" "this" {
+  length = 2
+}
