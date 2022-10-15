bddf0c44ac96286d401fb55e4204343faa16959e
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-10-16 01:39:19 +0900

    ADD: rds module (for reference)

---
+++ 20terraformer/terraform-manifests/rds_module/rds-module.tf
@@ -0,0 +1,70 @@
+# Create AWS RDS Database
+module "rdsdb" {
+  source  = "terraform-aws-modules/rds/aws"
+  version = "5.1.0"
+
+  identifier = var.db_instance_identifier
+
+  db_name  = var.db_name # Initial Database Name
+  username = var.db_username
+  password = var.db_password
+  port     = 5432
+
+
+  multi_az               = true
+  subnet_ids             = module.vpc.database_subnets
+  vpc_security_group_ids = [module.rds_aurora_sg.security_group_id]
+  create_db_subnet_group = true
+  # db_subnet_group_name = module.vpc.database_subnet_group_name
+  create_random_password = false
+  # All available versions: http://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_MySQL.html#MySQL.Concepts.VersionMgmt
+  engine               = "postgres"
+  engine_version       = "14.1"
+  family               = "postgres14" # DB parameter group
+  major_engine_version = "14"         # DB option group
+  instance_class       = "db.t3.medium"
+
+  allocated_storage     = 20
+  max_allocated_storage = 100
+  storage_encrypted     = false
+
+
+  maintenance_window              = "Mon:00:00-Mon:03:00"
+  backup_window                   = "03:00-06:00"
+  enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
+  create_cloudwatch_log_group     = true
+
+  backup_retention_period = 0
+  skip_final_snapshot     = true
+  deletion_protection     = false
+
+  performance_insights_enabled          = true
+  performance_insights_retention_period = 7
+  create_monitoring_role                = true
+  monitoring_interval                   = 60
+
+  parameters = [
+    {
+      name  = "autovacuum"
+      value = 1
+    },
+    {
+      name  = "client_encoding"
+      value = "utf8"
+    }
+  ]
+
+  tags = local.common_tags
+  db_instance_tags = {
+    "Sensitive" = "high"
+  }
+  db_option_group_tags = {
+    "Sensitive" = "low"
+  }
+  db_parameter_group_tags = {
+    "Sensitive" = "low"
+  }
+  db_subnet_group_tags = {
+    "Sensitive" = "high"
+  }
+}
