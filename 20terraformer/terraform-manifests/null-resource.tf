43b88d066cc97c14afa2499d1eaf4d4e2270575a
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-10-27 11:17:05 +0900

    REFACTOR: restructure root module

--- 20terraformer/terraform-manifests/null-resource.tf
+++
@@ -1,9 +0,0 @@
-# Create a Null Resource and provisioners
-
-resource "null_resource" "name" {
-  # Local Exec Provisioner: local-exec provisioner (Create-Time Provisioner - Triggered during Create Resource)
-  provisioner "local-exec" {
-    command     = "echo VPC created on `date` and VPC ID: ${module.custom_vpc.vpc_id} >> creation-time-vpc-id.txt"
-    working_dir = "local-exec-output-files/"
-  }
-}
--- 20terraformer/terraform-manifests/versions.tf
+++ 20terraformer/terraform-manifests/versions.tf
@@ -37,3 +37,12 @@ provider "aws" {
 resource "random_pet" "this" {
   length = 2
 }
+
+# Create a Null Resource and provisioners
+resource "null_resource" "name" {
+  # Local Exec Provisioner: local-exec provisioner (Create-Time Provisioner - Triggered during Create Resource)
+  provisioner "local-exec" {
+    command     = "echo VPC created on `date` and VPC ID: ${module.custom_vpc.vpc_id} >> creation-time-vpc-id.txt"
+    working_dir = "local-exec-output-files/"
+  }
+}
